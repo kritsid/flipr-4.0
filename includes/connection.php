193d8c5389ff9e628e619cf496858a99c0c0cd8d
kritika aggarwal <43928250+kritsid@users.noreply.github.com>
2020-07-19 15:10:23 +0530

    Add files via upload

---
+++ includes/connection.php
@@ -0,0 +1,5 @@
+<?php
+$conn=mysqli_connect("localhost","root","","laundry");
+// $conn = mysqli_connect("sql110.epizy.com","epiz_25594436","mAx91EGqMCfPxT","epiz_25594436_laundry")
+// epiz_25594436_laundry	epiz_25594436	(Your vPanel Password)	sql110.epizy.com    
+?>
---
+++ includes/navbar.php
@@ -0,0 +1,22 @@
+<head>
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div  id="navbarText">
+
+    <img src="../images/logo.png" style="width:40%;height:150px;" >
+
+    <span class="navbar-text" style="float: right">
+     MY LAUNDRY
+    </span>
+    </div>
+
+  </nav>
+
+<script src="../js/jquery-3.4.1.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
+</body>
