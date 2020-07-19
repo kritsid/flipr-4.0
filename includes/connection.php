5d8ce9632d6780ae484eef8439a2367261bd42d9
kritika aggarwal <43928250+kritsid@users.noreply.github.com>
2020-07-19 15:34:58 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,11 @@
 <?php
-$conn=mysqli_connect("localhost","root","","laundry");
+
+localhost = "ec2-52-70-15-120.compute-1.amazonaws.com"
+database = "deunf05vf3mv22"
+user_root = "hswhatfjfemdro"
+password = "ee13ed44022f4d69348ee636325618f68c0c442a0344b8e5f5ca667506c319d6"
+$conn=mysqli_connect(localhost,user_root,password,database);
+//$conn=mysqli_connect("localhost","root","","laundry");
 // $conn = mysqli_connect("sql110.epizy.com","epiz_25594436","mAx91EGqMCfPxT","epiz_25594436_laundry")
 // epiz_25594436_laundry	epiz_25594436	(Your vPanel Password)	sql110.epizy.com    
 ?>
